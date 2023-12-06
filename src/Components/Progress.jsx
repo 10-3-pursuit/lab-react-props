674c32bde6ecd19993c290a2f8630cf2f88a3d17
armandopires <armandopires@pursuit.org>
2023-12-06 16:35:49 -0500

    Update

--- src/Components/Progress.jsx
+++ src/Components/Progress.jsx
@@ -1,13 +1,13 @@
 function Progress(props) {
+  
   const { donations, targetAmount } = props;
-
   const fullAmount = donations.reduce((total, donation) => total + donation.amount, 0);
 
   return (
     <section className="progress">
       <h2>
-        Raised <span className="secondary">${fullAmount.toLocaleString()}</span> of
-        <span className="secondary">${targetAmount.toLocaleString()}</span>
+        Raised <span className="secondary">${fullAmount}</span> of
+        <span className="secondary">${targetAmount}</span>
       </h2>
     </section>
   );
