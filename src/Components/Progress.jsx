b65f65f4f1ca56c6dc9ebe7bf1542ab1697a73a4
Carlitos <carlitosdutan@pursuit.org>
2023-12-06 18:03:51 -0500

    Lab Done

--- src/Components/Progress.jsx
+++ src/Components/Progress.jsx
@@ -1,3 +1,12 @@
-export default function Progress() {
-  return null;
+function Progress(props) {
+  const totalAmount = props.donations.reduce((accumulator, donation) => accumulator + donation.amount, 0);
+  return(
+    <section className="progress">
+      <h2>
+        Raised <span className="secondary">{`$${totalAmount}`}</span> of <span className="secondary">{`$${props.targetAmount}`}</span>
+      </h2>
+    </section>
+  )
 }
+
+export default Progress;
