f7986a3b205aad93bf40b0faf1d22b8e10d67deb
Iris Beckham <irisdancer3@gmail.com>
2023-12-06 12:20:07 -0800

    form html

--- src/Components/DonationForm.jsx
+++ src/Components/DonationForm.jsx
@@ -2,6 +2,29 @@ export default function DonationForm(props) {
   return (
     <section className="donation">
       <h3>You could be donation <span className="secondary">#{props.number}!</span></h3>
+      <form>
+        <label htmlFor="name">Name
+          <input
+            id="name"
+            name="name"
+            type="text"
+            placeholder="Your name..." />
+        </label>
+        <label htmlFor="caption">Caption
+          <input
+            id="caption"
+            name="caption"
+            type="text"
+            placeholder="Add a brief message..." /></label>
+        <label htmlFor="amount">Amount
+          <input
+            id="amount"
+            name="amount"
+            type="number"
+            placeholder="0" />
+        </label>
+        <button>Donate!</button>
+      </form>
     </section>
   );
 }
