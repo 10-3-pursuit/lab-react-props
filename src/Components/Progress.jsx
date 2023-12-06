fd29c061c8f310f7cb1007baa4ff0651a6cf1d3e
KenCab123 <KennethCabral@pursuit.org>
2023-12-06 15:38:35 -0500

    finished

--- src/Components/Progress.jsx
+++ src/Components/Progress.jsx
@@ -1,3 +1,13 @@
-export default function Progress() {
-  return null;
+export default function Progress(props) {
+  return (
+    <section className="progress">
+      <h2>
+        Raised <span className="secondary">{props.donations.reduce((acc, curr) => {
+          acc += curr.amount
+          return acc
+        },0)}</span> of
+        <span className="secondary"> {props.targetAmount} </span>
+      </h2>
+    </section>
+  );
 }
