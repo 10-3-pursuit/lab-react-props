7655738502eb158657652c95bb5517a5692e2144
Jennifer <jenniferpeterson@pursuit.org>
2023-12-06 22:32:53 -0500

    completed Progress and Recent Donations

--- src/Components/RecentDonations.jsx
+++ src/Components/RecentDonations.jsx
@@ -1,3 +1,26 @@
-export default function RecentDonations() {
-  return null;
+export default function RecentDonations({ donations }) {
+  const amount = donations.map((donation) => (
+    <li key={donation.name}>
+      <span>
+        {donation.name} donated ${donation.amount}
+      </span>
+      {donation.caption}
+    </li>
+  ));
+
+  // const amount = donations.map((donation) => (
+  //   <>
+  //     <li style={{ listStyle: "none", color: "green" }} key={donation.name}>
+  //       {donation.name} donated ${donation.amount}
+  //     </li>
+  //     <p>{donation.caption}</p>
+  //   </>
+  // ));
+  // // console.log(amount);
+  return (
+    <>
+      <h2>Recent Donations</h2>
+      <ul>{amount}</ul>
+    </>
+  );
 }
