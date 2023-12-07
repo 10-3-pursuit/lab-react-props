bd27cb0286e0d2705aff8889b26aad5827a99594
juligarc91 <julissagarcia@pursuit.org>
2023-12-06 20:40:49 -0500

    added RecentDonations component to App.jsx

--- src/Components/RecentDonations.jsx
+++ src/Components/RecentDonations.jsx
@@ -1,3 +1,24 @@
-export default function RecentDonations() {
-  return null;
+// export default function RecentDonations() {
+//   return null;
+// }
+
+
+const RecentDonations = (props) => {
+  const { donations } = props;
+  return (
+    <section>
+      <h2>Recent Donations</h2>
+      <ul>
+        {donations.map((donation) => (
+          // key helps REACT understand what id 
+          <li key={donation.id}>
+            <span>{`${donation.name} donated $${donation.amount}`}</span>
+            <p>{donation.caption}</p>
+          </li>
+          ))}
+      </ul>
+    </section>
+  )
 }
+
+export default RecentDonations;
